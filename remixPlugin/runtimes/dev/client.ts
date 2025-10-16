ed17f4b6763b00000cdb1fefe400eded470c51be
benton <ben@benten.ca>
2025-10-16 02:26:23 -0700

    fix frame refresh bug

--- remixPlugin/runtimes/dev/client.ts
+++ remixPlugin/runtimes/dev/client.ts
@@ -16,7 +16,7 @@ async function resolveFrame(frameUrl: string) {
 	throw new Error(`Failed to fetch ${frameUrl}`)
 }
 
-const root = createFrame(document, {
+const root = createFrame(document.body, {
 	async loadModule(moduleUrl, name) {
 		let mod = await import(/* @vite-ignore */ moduleUrl + '?t=' + Date.now())
 		if (!mod) {
@@ -33,15 +33,18 @@ const root = createFrame(document, {
 
 if (import.meta.hot) {
 	import.meta.hot.on('vite:beforeFullReload', (payload) => {
-		resolveFrame(location.href).then((html) => {
-			root.render(html)
-				.then(() => console.debug('[vono]: root frame reloaded'))
-				.catch((error) => {
-					import.meta.hot!.send("frame-reload-error", { message: error.message })
-					location.reload()
-				})
+		requestAnimationFrame(() => {
+			resolveFrame(location.href).then((html) => {
+				root.render(html)
+					.then(() => console.debug('[vono] root frame reloaded.'))
+					.catch((error) => {
+						import.meta.hot!.send("frame-reload-error", { message: error.message })
+						location.reload()
+					})
+			})
 		})
-		console.debug('[vono]: reloading root frame...')
+
+		console.debug('[vono] reloading root frame...')
 		payload.path = 'prevent-full-reload.html'
 	})
 }
